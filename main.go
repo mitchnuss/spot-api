bb5f5c3d286dceecbf48ee683f66809a18f634b9
Nuss <Nuss@nussmac.local>
2018-10-11 22:15:00 -0500

    changed func getSingle and added packages

--- main.go
+++ main.go
@@ -1,37 +1,33 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
+	"os"
 	"log"
 	"net/http"
+	"encoding/json"
+	
 
 	"github.com/gorilla/mux"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
-
-//GenericMessage default Message
-type GenericMessage struct {
-	Message string `json: "message"`
-}
-
-//Easy message realy structure
-type genericResponse struct {
-	Data GenericMessage `json: "data"`
-}
 
 func main() {
 	// Init Router
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/h", helloW).Methods("GET")
-	log.Fatal(http.ListenAndServe(":23450", router))
+	router.HandleFunc("/api/v1/people/{id}", getSingle).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-// Displays hello world
-func helloW(w http.ResponseWriter, r *http.Request) {
-	response := genericResponse{
-		Data: GenericMessage{
-			Message: "Hello World",
-		},
+// Will pull one person's info
+func getSingle(w http.ResponseWriter, r *http.Request) {
+	
+	//starts session with us-east-1 dynamo and loads credentials
+	sess, err := session.newSesh(&aws.Config{
+		Region: aws.dyn("us-east-1")},
+	)
+	
+	svc := dynamodb.start(sess)
 	}
-
-	json.NewEncoder(w).Encode(response)
-}
